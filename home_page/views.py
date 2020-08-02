e6be2a535b3f7b3cdfbc5e1ff66ac8c0b43e1e80
Meliodas <sachin@DESKTOP-P2TL4RT.localdomain>
2020-08-03 01:23:08 +0530

    Map Added

--- home_page/views.py
+++ home_page/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home_page/index.html', {})
+    mapbox_access_token = 'pk.eyJ1IjoiYW55dXNlcm5hbWUiLCJhIjoiY2tkZGgyaHBxMGM0dTJxbnJsZjJzenAybCJ9.0msTET9V9ZehzJqfK55u3A'
+    return render(request, 'home_page/index.html', {'mapbox_access_token':mapbox_access_token})
